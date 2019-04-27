d813d94f94226a93212382576335272433b7495d
Sunday <sunday.oyeniyi6@gmail.com>
2019-04-27 16:32:43 +0000

    csv.py file has update. I add many libraries like matplotlib, numpy and pandas to display pie chart

--- analysis/csv.py
+++ analysis/csv.py
@@ -1,24 +1,72 @@
 # import os
 from os import path
+import ppprint
 import logging as lg
 
-def launch_analysis(data_file):
+import pandas as pd
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 
-    path_to_file = path.join("data", data_file)
 
-    file_name = path.basename(path_to_file)
-    directory = path.dirname(path_to_file)
-    lg.info("Opening data file {} form directory '{}'".format(file_name, path_to_file))
+class SetOfParliamentMembers(object):
+    """docstring forSetOfParliamentMembers."""
 
-    try:
-        with open(path_to_file, 'r') as file:
-            preview = file.readline()
-            lg.debug("Yeah! We managed to read th efile. here is a preview: {%s}" % preview)
-    except FileNotFoundError as e:
-        lg.critical('Ow :( The file was not found. Here is the original message of the exception: {%s}' % e)
-    except:
-        lg.critical("Destination unknown")
+    def __init__(self, name):
+        self.name = name
 
+    def data_from_csv(self, csv_file):
+        self.dataframe = pd.read_csv(csv_file, sep=";")
+
+    def data_from_dataframe(self, dataframe):
+        self.dataframe = dataframe
+
+    def display_chart(self):
+        data = self.dataframe
+        female_mps = data[data.sexe == "F"]
+        male_mps = data[data.sexe == "H"]
+
+        counts = [len(female_mps), len(male_mps)]
+        counts = np.array(counts)
+        nb_mps = counts.sum()
+        proportions = counts / nb_mps
+
+        labels = ["Female ({})".format(counts[0]), "Male ({})".format(counts[1])]
+
+        fig, ax = plt.subplots()
+        ax.axis("equal")
+        ax.pie(
+                proportions,
+                labels=labels,
+                autopct="%1.f%%"
+            )
+        plt.title("{} ({} MPs)".format(self.name, nb_mps))
+        plt.show()
+
+    def split_by_political_party(self):
+        result = {}
+        data = self.dataframe
+
+        all_parties = data["parti_ratt_financier"].dropna().unique()
+
+        for party in all_parties:
+            data_subset = data[data.parti_ratt_financier == party]
+            subset = SetOfParliamentMembers("MPs from party '{}'".format(party))
+            subset.data_from_dataframe(data_subset)
+            result[party] = subset
+
+        return result
+
+def launch_analysis(data_file, by_party = False):
+    sopm = SetOfParliamentMembers("All MPs")
+    sopm.data_from_csv(path.join("data",data_file)
+    sopm.display_chart()
+
+    if by_party:
+        for party, s in sopm.split_by_political_party().items():
+            s.display_chart()
 
 
 def main():
